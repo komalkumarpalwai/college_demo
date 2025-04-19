e98bf35283ee12b486fa21aa5b6bef40f399953a
komal kumar <152193731+komalkumarpalwai@users.noreply.github.com>
2025-04-19 20:47:59 +0530

    Fix: resolve build issue by cleaning up package.json and reinstalling dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173, // You can change this if needed
-    open: true, // Optional: opens browser on npm run dev
+  preview: {
+    port: process.env.PORT || 4173,
+    host: '0.0.0.0',
   },
 })
